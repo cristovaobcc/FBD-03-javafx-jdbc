53446adb7d76f9d8a77cbd3062d84d026d0873d6
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-31 15:17:27 -0300

    Add pacote gui.util com classes utilitárias para view

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,31 @@
+package gui.util;
+/**
+ * 
+ */
+
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+/**
+ * Classe com métodos para exibir Alerts do JavaFX.
+ *
+ */
+public class Alerts {
+	
+	/**
+	 * Exibe um Alert do tipo inserido e com os dados inseridos.
+	 * @param title String
+	 * @param header String
+	 * @param content String
+	 * @param type {@link AlertType} é uma Enumeration.
+	 */
+	public static void showAlert(String title, String header, String content, AlertType type) {
+		Alert alert = new Alert(type);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.show();
+	}
+
+}
