25d49401b6190e4fd0a6b7bccbb0819eafb8071a
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-02 16:05:40 -0300

    Refatora DepartmenteListController.onBtNewAction()
    A finalidade é para que o método obtenha o stage atual onde o evento de
    clique de botão(new) ocorreu e tb chame o método implementado neste
    commit: createDialogForm. Esse método é que mostra o
    DepartmentForm.fxml.

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -3,19 +3,28 @@
  */
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -41,8 +50,9 @@ public class DepartmentListController implements Initializable {
 	private ObservableList<Department> obsList; // os Deptos serão carregados em um ObservableList.
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction()");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartmentService(DepartmentService departmentService) {
@@ -79,5 +89,32 @@ public class DepartmentListController implements Initializable {
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
 		
+	}
+	
+	
+	/**
+	 * Recebe o caminho de uma view fxml e recebe um Stage para criar uma janela de diálogo.
+	 * @param absoluteName String
+	 * @param parentStage Stage
+	 */
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			// Para carregar uma nova janela modal(que fica na frente e bloqueando a de baixo),
+			// temos que carregar um novo stage:
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL); // método que diz se a janela criada será modal ou terá outro comportamento.
+			dialogStage.showAndWait();
+			
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
