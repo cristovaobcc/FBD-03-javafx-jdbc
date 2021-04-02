788d1f3bc8975955b5f645f9b470017957d1f0be
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-02 11:27:38 -0300

    Refatora a classe DepartmentService para acessar os dados do DB
    Note a importância de se estruturar o sistema em camadas: aqui
    utilizamos o que foi feito em outro projeto para acessar os dados. Ou
    seja, não precisamos modificar mais nada do que foi feito antes e todo o
    código foi reaproveitado. Esse reaproveitamento pode ser visto como uma
    peça que se encaixa de forma organizada no sistema.

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -6,6 +6,8 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 /**
@@ -14,17 +16,16 @@ import model.entities.Department;
  */
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	/**
 	 * Devolve todos os Departments.
 	 * @return {@link List}< Department >
 	 */
 	public List<Department> findAll(){
-		// Dados mockados.
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Eletronics"));
-		return list;
+		
+		return dao.findAll();
+		
 	}
 
 }
