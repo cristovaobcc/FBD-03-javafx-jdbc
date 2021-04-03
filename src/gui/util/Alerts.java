b8b109417b817f3006d7e9bfbaf0c76ebcd40208
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-03 14:12:09 -0300

    Add método Alerts.showConfirmation()
    Devolve um valor do tipo Optional<ButtonType>. Também exibe um alert do
    tipo Confirmation ao usuário. Isso serve para saber se o usuário
    apertou no 'sim' ou no 'não'.

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -4,8 +4,11 @@ package gui.util;
  */
 
 
+import java.util.Optional;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 /**
  * Classe com métodos para exibir Alerts do JavaFX.
@@ -26,6 +29,26 @@ public class Alerts {
 		alert.setHeaderText(header);
 		alert.setContentText(content);
 		alert.show();
+	}
+	
+	
+	/**
+	 * Devolve um valor do tipo Optional<ButtonType>. Também exibe um alert do
+	 * tipo Confirmation ao usuário. Isso serve para saber se o usuário
+	 * apertou no 'sim' ou no 'não'.
+	 * @param title String Titulo do Alert
+	 * @param content String Conteúdo do Alert
+	 * @return Optional< ButtonType >
+	 */
+	public static Optional<ButtonType> showConfirmation(String title, String content){
+		
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		
+		return alert.showAndWait();
+		
 	}
 
 }
