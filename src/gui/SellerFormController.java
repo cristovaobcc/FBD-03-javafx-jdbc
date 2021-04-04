7c77f4d8c353207a6b0e558d0f513efe8e5bd2d9
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-04 15:38:28 -0300

    Add novos campos no SellerFormController
    Campos add:
    1) TextField & DatePicker attributes (Email, BirthDate, BaseSalary)
    
    2) Label error attributes (Email, BirthDate, BaseSalary)

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -17,6 +17,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -44,7 +45,26 @@ public class SellerFormController implements Initializable {
 	private TextField txtName;
 	
 	@FXML
+	private TextField txtEmail;
+	
+	@FXML
+	private DatePicker dpBirthDate;
+	
+	@FXML
+	private TextField txtBaseSalary;
+	
+	@FXML
 	private Label labelErrorName;
+	
+	@FXML
+	private Label labelErrorEmail;
+	
+	@FXML
+	private Label labelErrorBirthDate;
+	
+	@FXML
+	private Label labelErrorBaseSalary;
+	
 	
 	@FXML
 	private Button btSave;
