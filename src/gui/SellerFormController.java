477e881131d38a9e01644019fae201dba064bcde
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-04 17:45:14 -0300

    Refatora SellerFormController para salvar Seller no DB.

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -36,150 +38,171 @@ import model.services.DepartmentService;
 import model.services.SellerService;
 
 /**
- * Classe de controle de eventos da Gui SellerForm.fxml.
- * Serve tanto para carregar dados de departamentos, como para inserir dados deles.
+ * Classe de controle de eventos da Gui SellerForm.fxml. Serve tanto para
+ * carregar dados de departamentos, como para inserir dados deles.
  *
  */
 public class SellerFormController implements Initializable {
-			
+
 	private DepartmentService deptService;
-	
+
 	private SellerService sellerService;
-	
+
 	private Seller sellerEntity;
-	
+
 	// Lista de objetos interessados em receber o evento de mudança de dados.
 	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
-	
+
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private TextField txtEmail;
-	
+
 	@FXML
 	private DatePicker dpBirthDate;
-	
+
 	@FXML
 	private TextField txtBaseSalary;
-	
+
 	@FXML
 	private ComboBox<Department> comboBoxDepartment;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Label labelErrorEmail;
-	
+
 	@FXML
 	private Label labelErrorBirthDate;
-	
+
 	@FXML
 	private Label labelErrorBaseSalary;
-	
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
-	
+
 	private ObservableList<Department> obsList;
-	
+
 	/**
 	 * Configura um SellerService e um DepartmentService.
+	 * 
 	 * @param sellerService {@link SellerService}
-	 * @param deptService {@link DepartmentService}
+	 * @param deptService   {@link DepartmentService}
 	 */
 	public void setServices(SellerService sellerService, DepartmentService deptService) {
 		this.sellerService = sellerService;
-		this.deptService = deptService;	
+		this.deptService = deptService;
 	}
-	
+
 	/**
 	 * Configura um Seller.
+	 * 
 	 * @param entity {@link Seller}
 	 */
 	public void setSellerEntity(Seller entity) {
 		this.sellerEntity = entity;
 	}
-	
+
 	/**
-	 * Registra um {@link DataChangeListener} que pode "ouvir" as mudanças de 
-	 * uma instância desta classe.
+	 * Registra um {@link DataChangeListener} que pode "ouvir" as mudanças de uma
+	 * instância desta classe.
+	 * 
 	 * @param listener @DataChangeListener
 	 */
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
 	}
-	
+
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
-		
+
 		if (sellerEntity == null) {
 			throw new IllegalStateException("departmentEntity is null");
 		}
 		if (sellerService == null) {
 			throw new IllegalStateException("departmentService is null");
 		}
-		
+
 		try {
 			sellerEntity = getFormData();
 			sellerService.saveOrUpdate(sellerEntity);
-			notifyDataChangeListeners(); 
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
-			
-		} 
-		catch (ValidationException e) {
+
+		} catch (ValidationException e) {
 			setErrorMessages(e.getErrors());
-		}
-		catch (DbException e) {
+		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
-		
-	}
-	
-	/**
-	 * Executa o método DataChangeListener.onDataChanged() em cada listener inscrito na  
-	 * instância desta classe.
-	 */
-	private void notifyDataChangeListeners() {
-		
-		for (DataChangeListener dataChangeListener : dataChangeListeners) {
-			dataChangeListener.onDataChanged();
-		}
-		
+
 	}
 
 	/**
-	 * Obtém os dados da view SellerForm e devolve-os
-	 * configurados num Seller.
+	 * Executa o método DataChangeListener.onDataChanged() em cada listener inscrito
+	 * na instância desta classe.
+	 */
+	private void notifyDataChangeListeners() {
+
+		for (DataChangeListener dataChangeListener : dataChangeListeners) {
+			dataChangeListener.onDataChanged();
+		}
+
+	}
+
+	/**
+	 * Obtém os dados da view SellerForm e devolve-os configurados num Seller.
+	 * 
 	 * @return {@link Seller}
 	 */
 	private Seller getFormData() {
 
-		Seller dept = new Seller();
-		
+		Seller seller = new Seller();
+
 		ValidationException validationException = new ValidationException("Validation error");
-		
-		dept.setId(Utils.tryParseToInteger(txtId.getText()));
-		
+
+		seller.setId(Utils.tryParseToInteger(txtId.getText()));
+
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			validationException.addError("name", "Field can't be empty");
 		}
-		// TODO: tratar o restante dos erros para exibir ao usuário.
-		dept.setName(txtName.getText());
+
+		seller.setName(txtName.getText());
+
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			validationException.addError("email", "Field can't be empty");
+		}
+
+		seller.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			validationException.addError("birthDate", "Field can't be empty");
+		} else {
+			// pega o valor que está num DatePicker
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			seller.setBirthDate(Date.from(instant));
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			validationException.addError("baseSalary", "Field can't be empty");
+		}
+
+		seller.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+		seller.setDepartment(comboBoxDepartment.getValue());
 		
 		if (validationException.getErrors().size() > 0) {
 			throw validationException;
 		}
-		
-		return dept;
+
+		return seller;
 	}
 
 	@FXML
@@ -192,25 +215,23 @@ public class SellerFormController implements Initializable {
 		initializeNodes();
 	}
 
-	
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 70);
 		Constraints.setTextFieldDouble(txtBaseSalary);
 		Constraints.setTextFieldMaxLength(txtEmail, 60);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
-		
+
 		initializeComboBoxDepartment();
-		
+
 	}
-	
-	
+
 	/**
 	 * Popula os dados de Seller setado neste controller, para o formulário
 	 * SellerForm.fxml.
 	 */
 	public void updateFormData() {
-		
+
 		if (sellerEntity == null) {
 			throw new IllegalStateException("SellerEntity is null");
 		}
@@ -218,25 +239,24 @@ public class SellerFormController implements Initializable {
 		txtName.setText(sellerEntity.getName());
 		txtEmail.setText(sellerEntity.getEmail());
 		Locale.setDefault(Locale.US);
-		txtBaseSalary.setText(String.format("%.2f",sellerEntity.getBaseSalary()));
-		
+		txtBaseSalary.setText(String.format("%.2f", sellerEntity.getBaseSalary()));
+
 		if (sellerEntity.getBirthDate() != null) {
 			// Configura a data conforme local onde roda o programa.
-			dpBirthDate.setValue(LocalDate.ofInstant(sellerEntity.getBirthDate().toInstant(), ZoneId.systemDefault())  );
+			dpBirthDate.setValue(LocalDate.ofInstant(sellerEntity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
-		
+
 		if (sellerEntity.getDepartment() == null) {
-			
+
 			comboBoxDepartment.getSelectionModel().selectFirst();
-			
-		}else {
-			
+
+		} else {
+
 			comboBoxDepartment.setValue(sellerEntity.getDepartment());
 		}
-		
-		
+
 	}
-	
+
 	/**
 	 * Carrega os dados de Departmentos cadastrados p/ combobox.
 	 */
@@ -248,37 +268,45 @@ public class SellerFormController implements Initializable {
 		obsList = FXCollections.observableArrayList(list); // Carrega na ObservableList
 		comboBoxDepartment.setItems(obsList); // carrega na comboBox.
 	}
-	
-		
+
 	/**
 	 * Preenche labels de erros de formulários com as mensagens de erros.
+	 * 
 	 * @param errors {@link Map} < String, String >
 	 */
 	private void setErrorMessages(Map<String, String> errors) {
-		
+
 		Set<String> fields = errors.keySet();
 		
-		if (fields.contains("name")) { // chave correspondente à instrução validationException.addError("name", "Field can't be empty");
-			labelErrorName.setText(errors.get("name"));
-		} 
+		// chave correspondente à instrução validationException.addError("name", "Field
+		// can't be empty");
+		labelErrorName.setText(fields.contains("name") ? errors.get("name") : "");
+		
+		// chave correspondente à instrução validationException.addError("email", "Field
+		// can't be empty");
+		labelErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+						
+		// chave correspondente à instrução validationException.addError("baseSalary",
+		// "Field can't be empty");
+		labelErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
+
 	}
-	
+
 	/**
-	 * Inicia o comboBoxDepartment. 
+	 * Inicia o comboBoxDepartment.
 	 */
 	private void initializeComboBoxDepartment() {
-		Callback<ListView<Department>, ListCell<Department>> factory = 
-				lv -> new ListCell<Department>() {
-					
-					@Override
-					protected void updateItem(Department dept, boolean empty) {
-						super.updateItem(dept, empty);
-						setText(empty ? "" : dept.getName());
-					};
-				};
-				
-				comboBoxDepartment.setCellFactory(factory);
-				comboBoxDepartment.setButtonCell(factory.call(null));
+		Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() {
+
+			@Override
+			protected void updateItem(Department dept, boolean empty) {
+				super.updateItem(dept, empty);
+				setText(empty ? "" : dept.getName());
+			};
+		};
+
+		comboBoxDepartment.setCellFactory(factory);
+		comboBoxDepartment.setButtonCell(factory.call(null));
 	}
-	
+
 }
