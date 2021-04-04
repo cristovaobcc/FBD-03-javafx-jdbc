60f6951fa92e5029686ddb56b9fbfe9caf94c4d2
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-04 15:37:32 -0300

    Insere método Utils.formatDatePicker()

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -4,25 +4,28 @@
 package gui.util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 /**
  * Classe com métodos estáticos utilitários para Controllers de GUI JavaFx.
  *
  */
 public class Utils {
-	
+
 	/**
-	 * Devolve o stage a partir de objeto de evento passado como
-	 * argumento.
+	 * Devolve o stage a partir de objeto de evento passado como argumento.
 	 * 
 	 * @param event {@link ActionEvent}
 	 * @return {@link Stage}
@@ -30,16 +33,15 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		Node node = (Node) event.getSource();
 		Scene scene = node.getScene();
-		
+
 		return (Stage) scene.getWindow();
 		// Deixo abaixo outra forma de implementação:
 		// return (Stage )((Node) event.getSource()).getScene().getWindow();
 	}
-	
-	
+
 	/**
-	 * Converte um valor string  para inteiro. Caso falhe,
-	 * devolve null.
+	 * Converte um valor string para inteiro. Caso falhe, devolve null.
+	 * 
 	 * @param str String
 	 * @return Integer
 	 */
@@ -49,63 +51,98 @@ public class Utils {
 		} catch (NumberFormatException e) {
 			return null;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Método que preenche a célula de uma tableColumn com um formato de data.
+	 * 
 	 * @param <T>
 	 * @param tableColumn TableColumn<T, Date>
-	 * @param format String
+	 * @param format      String
 	 */
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
-		tableColumn.setCellFactory(
-				column -> {
-					TableCell<T, Date> cell = new TableCell<T, Date>() {
-						private SimpleDateFormat sdf = new SimpleDateFormat(format);
-						
-						@Override 
-						protected void updateItem(Date item, boolean empty) {
-							super.updateItem(item, empty);
-							if(empty) {
-								setText(null);
-							} else {
-								setText(sdf.format(item));
-							}
-						}
-					};
-					
-					return cell;
-				});
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+
+			return cell;
+		});
 	}
-	
+
 	/**
-	 * Método que preenche a célula de uma tableColumn com um número decimal de até decimalPlaces casas.
+	 * Método que preenche a célula de uma tableColumn com um número decimal de até
+	 * decimalPlaces casas.
+	 * 
 	 * @param <T>
-	 * @param tableColumn TableColumn<T, Double> tableColumn
+	 * @param tableColumn   TableColumn<T, Double> tableColumn
 	 * @param decimalPlaces int
 	 */
 	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
-		tableColumn.setCellFactory(
-				column ->
-					{
-						TableCell<T, Double> cell = new TableCell<T,Double>(){
-							
-							@Override
-							protected void updateItem(Double item, boolean empty) {
-								super.updateItem(item, empty);
-								if (empty) {
-									setText(null);
-								} else {
-									Locale.setDefault(Locale.US);
-									setText(String.format("%." + decimalPlaces + "f", item));
-								}
-							}
-						};
-						
-						return cell;
-					}				
-				);
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+
+			return cell;
+		});
 	}
-	
+
+
+	/**
+	 * Formata a data de DatePicker conforme o format.
+	 * 
+	 * @param datePicker {@link DatePicker}
+	 * @param format     String
+	 */
+	public static void formatDatePicker(DatePicker datePicker, String format) {
+
+		datePicker.setConverter(new StringConverter<LocalDate>() {
+
+			DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+
+			{
+				datePicker.setPromptText(format.toLowerCase());
+			}
+
+			@Override
+			public String toString(LocalDate date) {
+				if (date != null) {
+					return dateFormatter.format(date);
+				} else {
+					return "";
+				}
+			}
+
+			@Override
+			public LocalDate fromString(String string) {
+				if (string != null && !string.isEmpty()) {
+					return LocalDate.parse(string, dateFormatter);
+				} else {
+					return null;
+				}
+			}
+		});
+	}
 }
